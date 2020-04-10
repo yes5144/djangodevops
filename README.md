32c38451b37ce622fe2bb0d0c4b39e3844edd706
wangkui <wangkui@hoodinn.com>
2020-04-10 18:03:59 +0800

    api test

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 开发框架2.0使用说明：https://docs.bk.tencent.com/blueapps/USAGE.html
+
+python3 manage.py runserver appdev.paas-class.bktencent.com:80
--- home_application/views.py
+++ home_application/views.py
@@ -1,17 +1,24 @@
 # -*- coding: utf-8 -*-
+import urllib3
 from django.shortcuts import render
-from django.http import HttpResponse,JsonResponse
+from django.http import HttpResponse, JsonResponse
 
 
 # 开发框架中通过中间件默认是需要登录态的，如有不需要登录的，可添加装饰器login_exempt
 # 装饰器引入 from blueapps.account.decorators import login_exempt
 def hello(request):
-    data={
-        "msg":"welcome to buleking",
-        "data":"test data",
-        "status":200
+    data = {"msg": "welcome to buleking", "data": "test data", "status": 200}
+    url = 'https://paas-class.bktencent.com/api/c/compapi/v2/bk_paas/get_app_info/'
+    # url = 'https://api.oioweb.cn/api/weather.php'
+    params = {
+        "bk_app_code": "djangodevops",
+        "bk_app_secret": "371403e0-cfc6-4da5-8396-0535e14dc07e",
     }
-    return JsonResponse(data)
+    http = urllib3.PoolManager()
+    ret = http.request('GET', url, params)
+    print(type(ret), ret)
+    return HttpResponse(ret.data)
+    # return render(request, "home_application/hello.html")
 
 
 def home(request):
