2c9233fa2c31f1fb711aaabf77660de72d60bf15
wangkui <wangkui@hoodinn.com>
2020-05-16 16:40:03 +0800

    set tasks.html to index

--- home_application/urls.py
+++ home_application/urls.py
@@ -5,10 +5,11 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = (
-    url(r'^$', views.hello),
+    url(r'^$', views.tasks),
     url(r'^home/$', views.home),
     url(r'^tasks/$', views.tasks),
     url(r'^record/$', views.record),
+    # url(r'^get_biz_info/$', views.get_biz_info),
     url(r'^dev-guide/$', views.dev_guide),
     url(r'^contact/$', views.contact),
 )
